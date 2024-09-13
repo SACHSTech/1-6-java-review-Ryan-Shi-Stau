e7a2435a84cd644b3a301687c96af7bc05447d8c
Ryan-Shi-Stau <ryan.shi25@ycdsbk12.ca>
2024-09-13 16:32:07 -0400

    done code for 8

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,32 @@
 package gr11review.part1;
+
+import java.io.*;
+import java.lang.Math;
+
+public class Review8 {
+
+  public static void main(String[] args) throws IOException {
+    // declare reader
+    BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+
+    // declare variables
+    int int1 = 0;
+    int int2 = 0;
+    int int3 = 0;
+    int intTriple = 0;
+
+    for (int i = 0; i < 1000; i++) {
+      int1 = (int) (Math.random() * 10);
+      int2 = (int) (Math.random() * 10);
+      int3 = (int) (Math.random() * 10);
+
+      System.out.println(int1 + " " + int2 + " " + int3);
+
+      if (int1 == int2 && int2 == int3) {
+        intTriple++;
+      }
+    }
+    System.out.println(intTriple);
+
+  }
+}
